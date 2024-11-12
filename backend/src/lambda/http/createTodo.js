d43bde57596ed6cd5c1b117a3c0c12393cc09d53
Le Tien Loc <letienlocvn@gmail.com>
2024-11-12 19:43:31 +0700

    Initialize code - Test deploy - successful

--- backend/src/lambda/http/createTodo.js
+++ backend/src/lambda/http/createTodo.js
@@ -1,7 +1,7 @@
 
 export function handler(event) {
   const newTodo = JSON.parse(event.body)
-
+  
   // TODO: Implement creating a new TODO item
   return undefined
 }
--- backend/src/lambda/http/getTodos.js
+++ backend/src/lambda/http/getTodos.js
@@ -1,4 +1,7 @@
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+const dynamoDb = new DynamoDBDocumentClient();
 export function handler(event) {
   // TODO: Get all TODO items for a current user
+  
   return undefined
 }
