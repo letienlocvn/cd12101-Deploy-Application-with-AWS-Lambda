5c0ffc0b36c9bbe3426709b73aa9e24a999af973
Le Tien Loc <letienlocvn@gmail.com>
2024-11-12 23:50:22 +0700

    Implement lambda function

--- backend/src/lambda/http/createTodo.js
+++ backend/src/lambda/http/createTodo.js
@@ -1,8 +1,27 @@
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
 
-export function handler(event) {
-  const newTodo = JSON.parse(event.body)
-  
-  // TODO: Implement creating a new TODO item
-  return undefined
-}
+import { createLogger } from '../../utils/logger.mjs'
+import { getUserId } from '../utils.mjs'
+import { createTodo } from '../../businessLogic/todo.mjs'
 
+const logger = createLogger('createData')
+
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(cors({ credentials: true }))
+  .handler(async (event) => {
+    {
+      logger.info("Creating: ", event)
+      const newTodo = JSON.parse(event.body)
+      const userId = getUserId(event)
+      const newItem = await createTodo(newTodo, userId)
+      return {
+        statusCode: 201,
+        body: JSON.stringify({
+          item: newItem
+        })
+      }
+    }
+  })
